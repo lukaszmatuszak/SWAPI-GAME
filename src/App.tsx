0c230da7cda9370f289907669f6283f9c4adf712
Łukasz Matuszak <lukaszmatuszak.priv@gmail.com>
2020-02-19 15:17:09 +0100

    Added prettier.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <p>placeholder</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <p>placeholder</p>
+        </div>
+    );
 }
 
 export default App;
