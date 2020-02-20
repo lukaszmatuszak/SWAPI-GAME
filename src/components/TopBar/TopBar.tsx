1db44d8df34647d3af3a2fd5bed08aaaf7bab6a9
Łukasz Matuszak <lukaszmatuszak.priv@gmail.com>
2020-02-20 19:55:51 +0100

    Added redirection to home page on logo click.

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppBar, Box, Toolbar, Typography } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 import ChangeLanguageMenu from '../ChangeLanguageMenu/ChangeLanguageMenu';
 
@@ -7,9 +8,11 @@ const TopBar: React.FC = () => (
     <AppBar position="static">
         <Toolbar variant="dense">
             <Box flexGrow={1}>
-                <Typography variant="h6" color="inherit">
-                    SWAPI Game
-                </Typography>
+                <Link to="/">
+                    <Typography variant="h6" color="inherit">
+                        SWAPI Game
+                    </Typography>
+                </Link>
             </Box>
             <ChangeLanguageMenu />
         </Toolbar>
