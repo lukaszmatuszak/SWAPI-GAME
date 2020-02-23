f5d55068c26f79fa000f81c2c5dec2b80f5af408
Łukasz Matuszak <lukaszmatuszak.priv@gmail.com>
2020-02-23 22:28:03 +0100

    Updates

---
+++ precache-manifest.518bbc92df5107e62f10c08e5864bdfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f09a1ea6ce3afc7fe6f24551e1c476b",
+    "url": "/SWAPI-GAME/index.html"
+  },
+  {
+    "revision": "104d8d7da093e0eb6df4",
+    "url": "/SWAPI-GAME/static/css/main.ec9dd1c0.chunk.css"
+  },
+  {
+    "revision": "b95b5fa98905124f49cd",
+    "url": "/SWAPI-GAME/static/js/2.3a8988b5.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/SWAPI-GAME/static/js/2.3a8988b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "104d8d7da093e0eb6df4",
+    "url": "/SWAPI-GAME/static/js/main.a29d7acf.chunk.js"
+  },
+  {
+    "revision": "b458d6aec9c640b22b01",
+    "url": "/SWAPI-GAME/static/js/runtime-main.9160418c.js"
+  }
+]);
