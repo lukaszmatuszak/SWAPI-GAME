9cdd19122666fbaa12c280f762dbede511df5339
Łukasz Matuszak <lukaszmatuszak.priv@gmail.com>
2020-02-23 22:43:55 +0100

    Updates

--- precache-manifest.518bbc92df5107e62f10c08e5864bdfc.js
+++ precache-manifest.518bbc92df5107e62f10c08e5864bdfc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "db1d5ee99912d77ece2ef458eba5ef0c",
+    "revision": "6f09a1ea6ce3afc7fe6f24551e1c476b",
     "url": "/SWAPI-GAME/index.html"
   },
   {
-    "revision": "e4051422ba49749640b4",
+    "revision": "104d8d7da093e0eb6df4",
     "url": "/SWAPI-GAME/static/css/main.ec9dd1c0.chunk.css"
   },
   {
-    "revision": "ae0f0fca59eb376a009f",
-    "url": "/SWAPI-GAME/static/js/2.95fa4642.chunk.js"
+    "revision": "b95b5fa98905124f49cd",
+    "url": "/SWAPI-GAME/static/js/2.3a8988b5.chunk.js"
   },
   {
     "revision": "3adc01bea76e7956dc3633ee898f6936",
-    "url": "/SWAPI-GAME/static/js/2.95fa4642.chunk.js.LICENSE.txt"
+    "url": "/SWAPI-GAME/static/js/2.3a8988b5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e4051422ba49749640b4",
-    "url": "/SWAPI-GAME/static/js/main.8fe99f5e.chunk.js"
+    "revision": "104d8d7da093e0eb6df4",
+    "url": "/SWAPI-GAME/static/js/main.a29d7acf.chunk.js"
   },
   {
     "revision": "b458d6aec9c640b22b01",
